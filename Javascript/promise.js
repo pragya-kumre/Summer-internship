dfd85ab0631eb848ffd55d15df3c9692179f1862
pragya-kumre <pragya.kumre01@gmail.com>
2026-06-22 12:56:34 +0530

    notes

---
+++ Javascript/promise.js
@@ -0,0 +1,30 @@
+const promise=new Promise((res,rej)=>{
+    let s=true;//false
+    if(s){
+        res("fullfilled");
+    
+    }else{
+        rej("Rejected")
+
+    }
+});
+//console.log(promise);
+promise.then(result=>{
+    console.log(result);
+})
+.catch(error=>{
+    console.log(error);
+})
+.finally(()=>{
+    console.log("finally");
+})
+
+
+async function greet () {
+    return ("hello")
+};
+console.log(greet());
+
+function fetchdata (){
+    setTimeout
+}
